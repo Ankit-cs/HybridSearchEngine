3311880843357494ffccd2fb70fb4b80f6bc4356
Ankit-cs <ankitcareer018@gmail.com>
2026-07-17 16:14:35 +0530

    implement document metadata and text storage

---
+++ src/storage/document_store.py
@@ -0,0 +1,24 @@
+import json
+
+
+class DocumentStore:
+    def __init__(self):
+        self.docs = {}
+
+    def add(self, doc_id, title, url, text):
+        self.docs[str(doc_id)] = {
+            "title": title,
+            "url": url,
+            "text": text
+        }
+    
+    def get(self, doc_id):
+        return self.docs.get(str(doc_id))
+
+    def save(self, path):
+        with open(path, "w", encoding="utf-8") as f:
+            json.dump(self.docs, f)
+
+    def load(self, path):
+        with open(path, "r", encoding="utf-8") as f:
+            self.docs = json.load(f)
